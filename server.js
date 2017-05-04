1a1fa44046880c2bed0dbf1dd3f5ba7423fc957c
Mark Lagusker <mk.lager@gmail.com>
2017-05-04 00:04:36 -0700

    Init repo

---
+++ server.js
@@ -0,0 +1,14 @@
+/**
+ * Created by mlagusker on 5/3/17.
+ */
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+    res.send('Todo API Root');
+});
+
+app.listen(PORT, function () {
+    console.log('Express listening on port ' + PORT + '!');
+});
