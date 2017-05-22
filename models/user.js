dc2511e4388a5364655c89ee818a904e2b4e2563
Mark Lagusker <mk.lager@gmail.com>
2017-05-22 00:38:49 -0700

    Hash and salt password

--- models/user.js
+++ models/user.js
@@ -1,6 +1,9 @@
 /**
  * Created by mlagusker on 5/21/17.
  */
+var bcrypt = require('bcrypt');
+var _ = require('underscore');
+
 module.exports = function (sequelize, DataTypes) {
     return sequelize.define('user', {
         email: {
@@ -11,11 +14,25 @@ module.exports = function (sequelize, DataTypes) {
                 isEmail: true
             }
         },
+        salt: {
+            type: DataTypes.STRING
+        },
+        password_hash: {
+            type: DataTypes.STRING
+        },
         password: {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
                 len: [7, 100]
+            },
+            set: function (value) {
+                var salt = bcrypt.genSaltSync(10);
+                var hashedPassword = bcrypt.hashSync(value, salt);
+
+                this.setDataValue('password', value);
+                this.setDataValue('salt', salt);
+                this.setDataValue('password_hash', hashedPassword);
             }
         }
     }, {
@@ -24,6 +41,12 @@ module.exports = function (sequelize, DataTypes) {
                 if (typeof user.email === 'string') {
                     user.email = user.email.toLowerCase();
                 }
+            }
+        },
+        instanceMethods: {
+            toPublicJSON: function () {
+                var json = this.toJSON();
+                return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
             }
         }
     });
